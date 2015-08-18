8dcfde0f22e8a9ff3c63839071029c429408bd05
Esteban De La Fuente Rubio <esteban@delaf.cl>
2015-08-18 18:42:28 -0300

    Se agrega extensión layouts donde se movió el layout de la aplicación

--- website/webroot/index.php
+++ website/webroot/index.php
@@ -3,19 +3,19 @@
 /**
  * SowerPHP: Minimalist Framework for PHP
  * Copyright (C) SowerPHP (http://sowerphp.org)
- * 
+ *
  * Este programa es software libre: usted puede redistribuirlo y/o
  * modificarlo bajo los términos de la Licencia Pública General GNU
  * publicada por la Fundación para el Software Libre, ya sea la versión
  * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
  * misma.
- * 
+ *
  * Este programa se distribuye con la esperanza de que sea útil, pero
  * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
  * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
  * Consulte los detalles de la Licencia Pública General GNU para obtener
  * una información más detallada.
- * 
+ *
  * Debería haber recibido una copia de la Licencia Pública General GNU
  * junto a este programa.
  * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
@@ -38,7 +38,7 @@ define ('DIR_PROJECT', dirname(dirname(dirname(__FILE__))));
 // Extensiones que se utilizarán. Deberá ser Vendor/Extensión dentro de
 // DIR_FRAMEWORK/extensions o bien dentro de DIR_PROJECT/extensions, ejemplo:
 // $_EXTENSIONS = array('sowerphp/dev', 'sowerphp/general');
-$_EXTENSIONS = array('sowerphp/app', 'sowerphp/general');
+$_EXTENSIONS = array('sowerphp/app', 'sowerphp/general', 'sowerphp/layouts');
 
 // Iniciar bootstrap (proceso que prepara e inicia el proyecto)
 if (!@include(DIR_FRAMEWORK.'/lib/sowerphp/core/bootstrap.php')) {
